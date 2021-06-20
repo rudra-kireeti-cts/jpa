ae031c19f8590b0e0c6ecf943d5346d3cd523f87
rudra kireeti kumar <rudra kireeti kumar@kireeti>
2021-06-20 16:03:22 +0530

    day 1 - session 1 and 2

--- src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.cognizant.ormlearn.model.Country;
 import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class OrmLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
-	
+
 	private static CountryService countryService;
 
 	private static void testGetAllCountries() {
@@ -30,12 +31,57 @@ public class OrmLearnApplication {
 
 	}
 
-	public static void main(String[] args) {
+	private static void getAllCountriesTest(String code) throws CountryNotFoundException {
+
+		LOGGER.info("Start handson 6 of day 1 - session 1 and 2");
+
+		Country country = countryService.findCountryByCode(code);
+
+		LOGGER.debug("Country:{}", country);
+
+		LOGGER.info("End handson 6 of day 1 - session 1 and 2");
+
+	}
+
+	public static void testAddCountry() {
+		LOGGER.info("Start handson 7 of day 1 - session 1 and 2");
+
+		Country country = new Country("cs", "cognizant");
+		countryService.addCountry(country);
+
+		LOGGER.info("End handson 7 of day 1 - session 1 and 2");
+	}
+
+	private static void updateCountryTest(String code, String name) throws CountryNotFoundException {
+
+		LOGGER.info("Start handson 8 of day 1 - session 1 and 2");
+
+		countryService.updateCountry(code, name);
+
+		LOGGER.info("End handson 8 of day 1 - session 1 and 2");
+
+	}
+	private static void deleteCountryTest(String code) {
+
+		LOGGER.info("Start handson 9 of day 1 - session 1 and 2");
+
+		countryService.deleteCountry(code);
+
+		LOGGER.info("End handson 9 of day 1 - session 1 and 2");
+
+	}
+
+	public static void main(String[] args) throws CountryNotFoundException {
 		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
 
 		countryService = context.getBean(CountryService.class);
 		LOGGER.info("Inside main");
 		testGetAllCountries();
+		getAllCountriesTest("IN");
+		testAddCountry();
+		getAllCountriesTest("cs");
+		updateCountryTest("cs","cognizant pro");
+		deleteCountryTest("cs");
 
 	}
 
--- src/main/java/com/cognizant/ormlearn/model/Country.java
+++ src/main/java/com/cognizant/ormlearn/model/Country.java
@@ -7,10 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "country", schema = "ormlearn")
 
 public class Country {
